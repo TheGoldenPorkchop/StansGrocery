ef28ca0a27cccd9c76951e6632e9361d3d2a840a
TheGoldenPorkchop <angelnavamusic@gmail.com>
2025-05-09 20:35:04 -0600

    Init

--- StansGrocery/StansGrocery/StansGroceryForm.Designer.vb
+++ StansGrocery/StansGrocery/StansGroceryForm.Designer.vb
@@ -22,75 +22,75 @@ Partial Class StansGroceryForm
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.Button1 = New System.Windows.Forms.Button()
-        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-        Me.ListBox1 = New System.Windows.Forms.ListBox()
-        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
-        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.SearchTextBox = New System.Windows.Forms.TextBox()
+        Me.SearchButton = New System.Windows.Forms.Button()
+        Me.FilterComboBox = New System.Windows.Forms.ComboBox()
+        Me.DisplayListBox = New System.Windows.Forms.ListBox()
+        Me.FilterByAisleRadioButton = New System.Windows.Forms.RadioButton()
+        Me.FilterByCategoryRadioButton = New System.Windows.Forms.RadioButton()
         Me.GroupBox1 = New System.Windows.Forms.GroupBox()
         Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
         '
-        'TextBox1
+        'SearchTextBox
         '
-        Me.TextBox1.Location = New System.Drawing.Point(37, 58)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 22)
-        Me.TextBox1.TabIndex = 0
+        Me.SearchTextBox.Location = New System.Drawing.Point(12, 145)
+        Me.SearchTextBox.Name = "SearchTextBox"
+        Me.SearchTextBox.Size = New System.Drawing.Size(100, 22)
+        Me.SearchTextBox.TabIndex = 0
         '
-        'Button1
+        'SearchButton
         '
-        Me.Button1.Location = New System.Drawing.Point(188, 41)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(75, 23)
-        Me.Button1.TabIndex = 1
-        Me.Button1.Text = "Button1"
-        Me.Button1.UseVisualStyleBackColor = True
+        Me.SearchButton.Location = New System.Drawing.Point(118, 145)
+        Me.SearchButton.Name = "SearchButton"
+        Me.SearchButton.Size = New System.Drawing.Size(75, 23)
+        Me.SearchButton.TabIndex = 1
+        Me.SearchButton.Text = "Search"
+        Me.SearchButton.UseVisualStyleBackColor = True
         '
-        'ComboBox1
+        'FilterComboBox
         '
-        Me.ComboBox1.FormattingEnabled = True
-        Me.ComboBox1.Location = New System.Drawing.Point(16, 120)
-        Me.ComboBox1.Name = "ComboBox1"
-        Me.ComboBox1.Size = New System.Drawing.Size(121, 24)
-        Me.ComboBox1.TabIndex = 2
+        Me.FilterComboBox.FormattingEnabled = True
+        Me.FilterComboBox.Location = New System.Drawing.Point(311, 52)
+        Me.FilterComboBox.Name = "FilterComboBox"
+        Me.FilterComboBox.Size = New System.Drawing.Size(477, 24)
+        Me.FilterComboBox.TabIndex = 2
         '
-        'ListBox1
+        'DisplayListBox
         '
-        Me.ListBox1.FormattingEnabled = True
-        Me.ListBox1.ItemHeight = 16
-        Me.ListBox1.Location = New System.Drawing.Point(311, 82)
-        Me.ListBox1.Name = "ListBox1"
-        Me.ListBox1.Size = New System.Drawing.Size(477, 356)
-        Me.ListBox1.TabIndex = 3
+        Me.DisplayListBox.FormattingEnabled = True
+        Me.DisplayListBox.ItemHeight = 16
+        Me.DisplayListBox.Location = New System.Drawing.Point(311, 82)
+        Me.DisplayListBox.Name = "DisplayListBox"
+        Me.DisplayListBox.Size = New System.Drawing.Size(477, 356)
+        Me.DisplayListBox.TabIndex = 3
         '
-        'RadioButton1
+        'FilterByAisleRadioButton
         '
-        Me.RadioButton1.AutoSize = True
-        Me.RadioButton1.Location = New System.Drawing.Point(6, 21)
-        Me.RadioButton1.Name = "RadioButton1"
-        Me.RadioButton1.Size = New System.Drawing.Size(109, 20)
-        Me.RadioButton1.TabIndex = 4
-        Me.RadioButton1.TabStop = True
-        Me.RadioButton1.Text = "RadioButton1"
-        Me.RadioButton1.UseVisualStyleBackColor = True
+        Me.FilterByAisleRadioButton.AutoSize = True
+        Me.FilterByAisleRadioButton.Checked = True
+        Me.FilterByAisleRadioButton.Location = New System.Drawing.Point(6, 21)
+        Me.FilterByAisleRadioButton.Name = "FilterByAisleRadioButton"
+        Me.FilterByAisleRadioButton.Size = New System.Drawing.Size(58, 20)
+        Me.FilterByAisleRadioButton.TabIndex = 4
+        Me.FilterByAisleRadioButton.TabStop = True
+        Me.FilterByAisleRadioButton.Text = "Aisle"
+        Me.FilterByAisleRadioButton.UseVisualStyleBackColor = True
         '
-        'RadioButton2
+        'FilterByCategoryRadioButton
         '
-        Me.RadioButton2.AutoSize = True
-        Me.RadioButton2.Location = New System.Drawing.Point(6, 61)
-        Me.RadioButton2.Name = "RadioButton2"
-        Me.RadioButton2.Size = New System.Drawing.Size(109, 20)
-        Me.RadioButton2.TabIndex = 5
-        Me.RadioButton2.TabStop = True
-        Me.RadioButton2.Text = "RadioButton2"
-        Me.RadioButton2.UseVisualStyleBackColor = True
+        Me.FilterByCategoryRadioButton.AutoSize = True
+        Me.FilterByCategoryRadioButton.Location = New System.Drawing.Point(6, 61)
+        Me.FilterByCategoryRadioButton.Name = "FilterByCategoryRadioButton"
+        Me.FilterByCategoryRadioButton.Size = New System.Drawing.Size(83, 20)
+        Me.FilterByCategoryRadioButton.TabIndex = 5
+        Me.FilterByCategoryRadioButton.Text = "Category"
+        Me.FilterByCategoryRadioButton.UseVisualStyleBackColor = True
         '
         'GroupBox1
         '
-        Me.GroupBox1.Controls.Add(Me.RadioButton2)
-        Me.GroupBox1.Controls.Add(Me.RadioButton1)
+        Me.GroupBox1.Controls.Add(Me.FilterByCategoryRadioButton)
+        Me.GroupBox1.Controls.Add(Me.FilterByAisleRadioButton)
         Me.GroupBox1.Location = New System.Drawing.Point(16, 173)
         Me.GroupBox1.Name = "GroupBox1"
         Me.GroupBox1.Size = New System.Drawing.Size(200, 100)
@@ -98,17 +98,17 @@ Partial Class StansGroceryForm
         Me.GroupBox1.TabStop = False
         Me.GroupBox1.Text = "GroupBox1"
         '
-        'Form1
+        'StansGroceryForm
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
         Me.Controls.Add(Me.GroupBox1)
-        Me.Controls.Add(Me.ListBox1)
-        Me.Controls.Add(Me.ComboBox1)
-        Me.Controls.Add(Me.Button1)
-        Me.Controls.Add(Me.TextBox1)
-        Me.Name = "Form1"
+        Me.Controls.Add(Me.DisplayListBox)
+        Me.Controls.Add(Me.FilterComboBox)
+        Me.Controls.Add(Me.SearchButton)
+        Me.Controls.Add(Me.SearchTextBox)
+        Me.Name = "StansGroceryForm"
         Me.Text = "Form1"
         Me.GroupBox1.ResumeLayout(False)
         Me.GroupBox1.PerformLayout()
@@ -117,11 +117,11 @@ Partial Class StansGroceryForm
 
     End Sub
 
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents Button1 As Button
-    Friend WithEvents ComboBox1 As ComboBox
-    Friend WithEvents ListBox1 As ListBox
-    Friend WithEvents RadioButton1 As RadioButton
-    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents SearchTextBox As TextBox
+    Friend WithEvents SearchButton As Button
+    Friend WithEvents FilterComboBox As ComboBox
+    Friend WithEvents DisplayListBox As ListBox
+    Friend WithEvents FilterByAisleRadioButton As RadioButton
+    Friend WithEvents FilterByCategoryRadioButton As RadioButton
     Friend WithEvents GroupBox1 As GroupBox
 End Class
--- StansGrocery/StansGrocery/StansGroceryForm.vb
+++ StansGrocery/StansGrocery/StansGroceryForm.vb
@@ -1,5 +1,206 @@
-﻿Public Class StansGroceryForm
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+﻿'Angel Nava
+'Spring 2025
+'RCET2265
+'Graphics Example
+'link
+'This code is pretty broken
+Option Strict On
+Option Explicit On
+Imports System.IO
+Public Class StansGroceryForm
+
+    Dim item$(,)
+
+    Sub ReadfromFile()
+        Dim filePath As String = "..\..\Grocery.txt"
+        Dim fileNumber As Integer = FreeFile()
+        Dim currentRecord As String = ""
+        Dim temp() As String 'use for spliting custom data
+
+        Try
+            FileOpen(fileNumber, filePath, OpenMode.Input)
+
+            Do Until EOF(fileNumber)
+                Input(fileNumber, currentRecord) 'reads exactly one record
+                If currentRecord <> "" Then 'ignores blank records
+                    temp = Split(currentRecord, ",")
+                    'DisplayListBox.Items.Add(currentRecord)
+
+                    If temp.Length = 4 Then 'ignores corrupted/malformed records
+                        temp(0) = Replace(temp(0), "$$ITM", "")
+                        temp(1) = Replace(temp(1), "##LOC", "")
+                        temp(2) = Replace(temp(2), "%%CAT", "")
+                        DisplayListBox.Items.Add(temp(0))
+                        WritetoFile(temp(0)) 'Item
+                        WritetoFile(temp(1)) 'Aisle
+                        WritetoFile(temp(2)) 'Category
+                    End If
+
+                End If
+            Loop
+
+            FileClose(fileNumber)
+        Catch woag As FileNotFoundException
+            MsgBox("Woag")
+        Catch ex As Exception
+            MsgBox(ex.Message & vbNewLine & ex.StackTrace & vbNewLine & ex.GetBaseException.ToString)
+        End Try
+    End Sub
+
+    Sub WritetoFile(newRecord As String, Optional insertLine As Boolean = False)
+        Dim filePath As String = "..\..\Grocery.txt"
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            FileOpen(fileNumber, filePath, OpenMode.Append)
+            Write(fileNumber, newRecord)
+
+            If insertLine Then
+                WriteLine(fileNumber)
+            End If
+
+            FileClose(fileNumber)
+        Catch ex As Exception
+            MsgBox("err")
+        End Try
 
     End Sub
+
+    Sub LoadItemData()
+        Dim filePath As String = "..\..\Grocery.txt"
+        Dim fileNumber As Integer = FreeFile()
+        Dim currentRecord As String
+        Dim InvalidFileName As Boolean = True
+        Dim _Items(NumberOfItems(filePath) - 1, 2) As String 'array for Item data
+        Dim currentItem As Integer = 0
+
+        Do
+
+            Try
+                FileOpen(fileNumber, filePath, OpenMode.Input)
+                InvalidFileName = False
+                Do Until EOF(fileNumber)
+                    Input(fileNumber, currentRecord)
+                    _Items(currentItem, 0) = currentRecord 'Name of Item
+                    Input(fileNumber, currentRecord)
+                    _Items(currentItem, 1) = currentRecord 'Aisle
+                    Input(fileNumber, currentRecord)
+                    _Items(currentItem, 2) = currentRecord 'Category
+                    Input(fileNumber, currentRecord) 'empty to discard
+
+                    currentItem += 1
+
+                Loop
+                FileClose(fileNumber)
+                'MsgBox($"There are {NumberOfItems(filePath)} Items")
+
+            Catch woag As FileNotFoundException
+                InvalidFileName = False
+                MsgBox($"The current file is {filePath}")
+
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        Loop While InvalidFileName
+
+        items(_Items)
+
+    End Sub
+
+    Sub DisplayFilteredData()
+        Dim _items(,) As String = items()
+        FilterComboBox.Items.Clear()
+        'Makes sure the array has schtuff
+        If _items IsNot Nothing Then
+            For row = 0 To _items.GetUpperBound(0) 'check every row
+                For column = 0 To _items.GetUpperBound(1) 'check every column
+                    If InStr(_items(row, column), SearchTextBox.Text) > 0 Then 'search withing string
+                        Select Case True
+                            Case FilterByAisleRadioButton.Checked
+                                If Not FilterComboBox.Items.Contains(_items(row, 1)) Then 'don't add duplicates
+                                    FilterComboBox.Items.Add(_items(row, 1))
+                                End If
+                            Case FilterByCategoryRadioButton.Checked
+                                If Not FilterComboBox.Items.Contains(item$(row, 2)) Then 'don't add duplicates
+                                    FilterComboBox.Items.Add(item$(row, 2))
+                                End If
+
+                        End Select
+
+                    End If
+                Next
+            Next
+
+            FilterComboBox.Sorted = True
+
+            If FilterComboBox.Items.Count >= 1 Then 'if there are results Filter the first one
+                FilterComboBox.SelectedIndex() = 0
+            End If
+        End If
+    End Sub
+
+    Function NumberOfItems(fileName As String) As Integer
+        Dim count As Integer = 0
+        Dim filePath As String = "..\..\Grocery.txt"
+        Dim fileNumber As Integer = FreeFile()
+
+        Try
+            FileOpen(fileNumber, fileName, OpenMode.Input)
+            While Not EOF(fileNumber)
+                LineInput(fileNumber)
+                count += 1
+            End While
+            FileClose(fileNumber)
+        Catch ex As Exception
+            'count = -1
+        End Try
+
+        Return count
+    End Function
+
+    Function items(Optional itemData(,) As String = Nothing) As String(,)
+        Static _items(,) As String
+        If itemData IsNot Nothing Then
+            _items = itemData
+        End If
+        Return _items
+    End Function
+
+    Sub FillListBox(searchColumn As Integer)
+        Dim _items(,) As String = items()
+        DisplayListBox.Items.Clear()
+
+        If _items IsNot Nothing Then
+            For row = 0 To _items.GetUpperBound(0) 'check every row
+                If _items(row, searchColumn) = FilterComboBox.SelectedItem.ToString() Then
+                    DisplayListBox.Items.Add($"{_items(row, 0) & "," & _items(row, 1).PadRight(25)} {_items(row, 2)}")
+                End If
+            Next
+
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles SearchButton.Click
+        ReadfromFile()
+    End Sub
+
+    Private Sub StansGrocery_Load(sender As Object, e As EventArgs) Handles Me.Load
+        LoadItemData()
+        ReadfromFile()
+    End Sub
+
+    Private Sub SelectComboBox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles FilterComboBox.SelectedIndexChanged
+        Select Case True
+            Case FilterByAisleRadioButton.Checked
+                FillListBox(1)
+            Case FilterByCategoryRadioButton.Checked
+                FillListBox(2)
+        End Select
+
+    End Sub
+
+    Private Sub FilterRadioButtons_CheckedChanged(sender As Object, e As EventArgs) Handles FilterByAisleRadioButton.CheckedChanged, FilterByCategoryRadioButton.CheckedChanged
+        DisplayFilteredData()
+    End Sub
+
 End Class
